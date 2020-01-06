a422a4258da4b5e10162a0ab5ec74ceead9a6f7f
Masayoshi.Nomura <masayoshi.nomura1983@gmail.com>
2020-01-06 17:05:31 +0900

    lambdaテスト lambda上でフィボナッチ数をjsonで返却

--- src/php/index.php
+++ src/php/index.php
@@ -1,1 +1,33 @@
-<?php phpinfo();
+<?php
+
+/**
+ * Class Test
+ *
+ * AWS Lambda上でphpを使用してJsonをResponseできるか確認
+ */
+class Test {
+    /**
+     * @param $n
+     * @return int
+     */
+    public function fib($n) {
+        if ($n == 0) {
+            return 0;
+        } else if ($n == 1) {
+            return 1;
+        }
+
+        // 再帰
+        return self::fib($n-1) + self::fib($n-2);
+    }
+}
+
+for ($i = 0; $i <= 10; $i++) {
+    $fib[] = Test::fib($i);
+}
+
+$json = json_encode($fib, JSON_UNESCAPED_UNICODE);
+echo $json;
+
+
+
