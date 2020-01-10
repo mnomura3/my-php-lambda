d9f1730c2acad9623c0b30bf4f9ef36fd80e3fa7
Masayoshi.Nomura <masayoshi.nomura1983@gmail.com>
2020-01-10 14:24:53 +0900

    分岐

--- src/php/index.php
+++ src/php/index.php
@@ -26,8 +26,12 @@ for ($i = 0; $i <= 10; $i++) {
     $fib[] = Test::fib($i);
 }
 
-$json = json_encode($fib, JSON_UNESCAPED_UNICODE);
-echo $json;
+if (!empty($fib)) {
+    $json = json_encode($fib, JSON_UNESCAPED_UNICODE);
+    echo $json;
+}
+// エラーをSQSへqueメッセージをSlack通知
+// todo ↓へ管理者へ通知その処理
 
 
 
