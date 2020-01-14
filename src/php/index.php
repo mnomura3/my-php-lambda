c08cfc07279197dddacc3636c8955b481c9c45a3
Masayoshi.Nomura <masayoshi.nomura1983@gmail.com>
2020-01-14 15:33:32 +0900

    微調整

--- src/php/index.php
+++ src/php/index.php
@@ -35,30 +35,31 @@ for ($i = 0; $i <= 10; $i++) {
 if (!empty($fib)) {
     $json = json_encode($fib, JSON_UNESCAPED_UNICODE);
     echo $json;
+} else {
+    // エラーをSQSへqueメッセージをSlack通知
+    // todo ↓へ管理者へ通知その処理
+
+    // キューの作成
+    $queueName = "SQS_QUEUE_NAME";
+
+    $client = new SqsClient([
+        'profile' => 'default',
+        'region' => 'ap-northeast-1',
+        'version' => '2012-11-05'
+    ]);
+
+    try {
+        $result = $client->createQueue(array(
+            'QueueName' => $queueName,
+            'Attributes' => array(
+                'DelaySeconds' => 5,
+                'MaximumMessageSize' => 4096, // 4 KB
+            ),
+        ));
+        var_dump($result);
+    } catch (AwsException $e) {
+        // output error message if fails
+        error_log($e->getMessage());
+    }
+
 }
-// エラーをSQSへqueメッセージをSlack通知
-// todo ↓へ管理者へ通知その処理
-
-// キューの作成
-$queueName = "SQS_QUEUE_NAME";
-
-$client = new SqsClient([
-    'profile' => 'default',
-    'region' => 'ap-northeast-1',
-    'version' => '2012-11-05'
-]);
-
-try {
-    $result = $client->createQueue(array(
-        'QueueName' => $queueName,
-        'Attributes' => array(
-            'DelaySeconds' => 5,
-            'MaximumMessageSize' => 4096, // 4 KB
-        ),
-    ));
-    var_dump($result);
-} catch (AwsException $e) {
-    // output error message if fails
-    error_log($e->getMessage());
-}
-
